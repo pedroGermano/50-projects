02ad0ef671b3d75fd2099e1434bf8363732b8ea6
Pedro Germano <pedrogermano232@gmail.com>
2023-07-08 22:09:03 -0300

    teste ok

--- rotating-animation/script.js
+++ rotating-animation/script.js
@@ -4,5 +4,3 @@ const container = document.querySelector('.container')
 
 open.addEventListener('click', () => container.classList.add('show-nav'))
 close.addEventListener('click', () => container.classList.remove('show-nav'))
-close.addEventListener('click', () => container.classList.remove('show-nav'))
-close.addEventListener('click', () => container.classList.remove('show-nav'))
